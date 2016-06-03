fa5212dcaad3cc8315f1768f75aaced2b4ddb8c1
Juan Carlos <juancarlos.segovia@gmail.com>
2016-06-03 18:48:59 +0200

    Created Discount class

---
+++ src/machine/types/Discount.java
@@ -0,0 +1,26 @@
+package machine.types;
+
+public class Discount {
+	private int numItems;
+	private int discountValue;
+	
+	public Discount(){
+		
+	}
+
+	public int getNumItems() {
+		return numItems;
+	}
+
+	public void setNumItems(int numItems) {
+		this.numItems = numItems;
+	}
+
+	public int getDiscountValue() {
+		return discountValue;
+	}
+
+	public void setDiscountValue(int discountValue) {
+		this.discountValue = discountValue;
+	}
+}
